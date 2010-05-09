b5a243b74afe6e765332a713123a717fd3c4781e
Walter McGinnis <walter@katipo.co.nz>
2010-05-09 20:34:55 +1200

    slight refactoring of url_for_translated helper to allow for easier overriding by apps.

--- lib/translations_controller_helpers.rb
+++ lib/translations_controller_helpers.rb
@@ -1,14 +1,20 @@
 module TranslationsControllerHelpers
   unless included_modules.include? TranslationsControllerHelpers
     def self.included(klass)
-      klass.send :helper_method, :url_for_translated
+      klass.send :helper_method, :url_for_translated, :target_action
     end
+    
+    def target_action(options)
+      options.delete(:action) || 'show'
+    end
+
     def url_for_translated(options = { })
       translated = options.delete(:translated) || @translated || @translatable
       translatable_params_name = options.delete(:translatable_params_name) || @translatable_params_name
+      target_action = target_action(options)
 
       defaults = { :id => translated,
-        :action => 'show',
+        :action => target_action,
         :controller => translatable_params_name.pluralize }
       url_for(defaults.merge(options))
     end
