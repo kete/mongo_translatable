00f67a9388c2e91372830271ae11db2785eecf10
Kieran Pilkington <kieran@katipo.co.nz>
2010-05-27 17:51:21 +1200

    Reorganise and get target_id to actually be functional

--- lib/translations_controller_helpers.rb
+++ lib/translations_controller_helpers.rb
@@ -4,33 +4,31 @@ module TranslationsControllerHelpers
       klass.send :helper_method, :url_for_translated, :target_action, :target_locale
     end
 
+    def target_locale(options = {})
+      translation = options.delete(:translation) || @translation
+      options.delete(:locale) || (translation.locale if translation) || I18n.locale
+    end
+
+    def target_controller(options = {})
+      translatable_params_name = options.delete(:translatable_params_name) || @translatable_params_name
+      options.delete(:controller) || translatable_params_name.pluralize || params[:controller]
+    end
+
     def target_action(options = {})
       options.delete(:action) || 'show'
     end
 
-    def target_locale(options = {})
-      options.delete(:locale) || (@translation.locale if @translation) || I18n.locale
-    end
-
-    def target_controller(options = {})
-      options.delete(:controller) || options.delete(:translatable_params_name).pluralize
-    end
-
     def target_id(options = {})
-      options.delete(:id)
+      translated = options.delete(:translated) || @translated || options.delete(:translatable) || @translatable
+      options.delete(:id) || translated
     end
 
     def url_for_translated(options = { })
-      translated = options.delete(:translated) || @translated || @translatable
-      translatable_params_name = options.delete(:translatable_params_name) || @translatable_params_name
-      options[:translatable_params_name] = translatable_params_name
-      options[:id] = translated
-
       defaults = {
         :locale => target_locale(options),
         :controller => target_controller(options),
         :action => target_action(options),
-        :id => target_id
+        :id => target_id(options)
       }
 
       url_for(defaults.merge(options))
